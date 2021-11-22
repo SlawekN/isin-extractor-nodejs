a8fab35d7339c1855a55a763ddd5c71b6607f65d
Michal Gosek <michal.gosek@almservices.pl>
2021-11-22 08:02:04 +0100

    moved content from parser pkg to indexer, removed parser pkg.

--- src/indexer/Parser.test.ts
+++ src/indexer/Parser.test.ts
@@ -4,7 +4,8 @@ import {ParserErrorEmptyJSONString, ParserErrorMissingProperties} from './Error'
 describe('Parser', () => {
   it('should convert valid JSON string describing single element to IsInIndexItem class Object', () => {
     const input = `{"figi": "test1", "isin": "test2", "companyname": "test3"}`;
-    const item = Parser.ToIsInIndexItem(input);
+    const parser = new Parser();
+    const item = parser.ConvertToIsInIndexItem(input);
 
     expect(item.figi).toBe("test1");
     expect(item.isIn).toBe("test2");
@@ -13,11 +14,15 @@ describe('Parser', () => {
 
   it ('should throw ParserErrorEmptyJSONString error for empty JSON string', () => {
     const input = "";
-    expect(() =>  Parser.ToIsInIndexItem(input)).toThrowError(ParserErrorEmptyJSONString);
+    const parser = new Parser();
+
+    expect(() =>  parser.ConvertToIsInIndexItem(input)).toThrowError(ParserErrorEmptyJSONString);
   });
 
   it ('should throw ParserErrorMissingProperties error for missing figi property in JSON string', () => {
     const input = `{"isin": "test2", "companyname": "test3"}`;
-    expect(() =>  Parser.ToIsInIndexItem(input)).toThrowError(ParserErrorMissingProperties);
+    const parser = new Parser();
+
+    expect(() =>  parser.ConvertToIsInIndexItem(input)).toThrowError(ParserErrorMissingProperties);
   })
 });
--- src/indexer/Parser.ts
+++ src/indexer/Parser.ts
@@ -1,4 +1,4 @@
-import { IsInIndexItem} from '../IsInIndexItem';
+import { IsInIndexItem} from './IsInIndexItem';
 import { ParserErrorEmptyJSONString, ParserErrorMissingProperties } from './Error';
 
 interface JSONBody {
@@ -8,20 +8,19 @@ interface JSONBody {
 }
 
 class Parser {
-  static hasUndefinedProperties(obj: JSONBody): boolean {
+  private static hasUndefinedProperties(obj: JSONBody): boolean {
     const missingISIN = obj.isin === undefined;
     const missingFigi = obj.figi ===  undefined;
     const missingCompanyName = obj.companyname === undefined;
     return missingISIN || missingFigi ||  missingCompanyName;
   }
 
-  static ToIsInIndexItem(_jsonText: string): IsInIndexItem {
-    // validation will be extracted to separate private func
-    if (!_jsonText.length)
+  public ConvertToIsInIndexItem(jsonText: string): IsInIndexItem {
+    if (!jsonText.length)
       throw ParserErrorEmptyJSONString;
 
-    const obj: JSONBody = JSON.parse(_jsonText);
-    const missingProperties: boolean = this.hasUndefinedProperties(obj)
+    const obj: JSONBody = JSON.parse(jsonText);
+    const missingProperties: boolean = Parser.hasUndefinedProperties(obj)
     if (missingProperties) {
       throw ParserErrorMissingProperties;
     }
