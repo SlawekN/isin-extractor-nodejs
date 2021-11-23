8161bb01b6faae176c02ebb662e9c780cc56b227
Michal Gosek <michal.gosek@almservices.pl>
2021-11-23 08:36:43 +0100

    added test case when invalid input is passed to IndexDocument method.

--- __tests__/SearchIndexer.test.ts
+++ __tests__/SearchIndexer.test.ts
@@ -4,6 +4,9 @@ import { appendFile } from 'fs';
 jest.mock('fs');
 
 describe('SearchIndexer', () => {
+  beforeAll(() => jest.spyOn(console, 'error').mockImplementation());
+  afterAll(() => jest.spyOn(console, 'error').mockRestore());
+
   it('should call appendFile once with: output.csv, test1,test2,test3, ErrorCallback', () => {
     const input = `{"figi": "test1", "isin": "test2", "companyname": "test3"}`;
     const indexer = new SearchIndexer();
@@ -11,4 +14,11 @@ describe('SearchIndexer', () => {
     expect(indexer.IndexDocument(input)).toBe('OK');
     expect(appendFile).toHaveBeenCalledTimes(1);
   });
+
+  it('should return NOT OK for passing empty string argument to IndexDocument method', () => {
+    const input = "";
+    const indexer = new SearchIndexer();
+
+    expect(indexer.IndexDocument(input)).toBe('NOT OK');
+  })
 });
