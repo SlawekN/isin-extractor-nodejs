01868c1fa4c74d90a6c8297995d2382c6022ddd9
Michal Gosek <michal.gosek@almservices.pl>
2021-11-24 11:31:22 +0100

    added two test cases for greater function.

---
+++ src/greeter.test.ts
@@ -0,0 +1,17 @@
+import { greeter } from './greeter';
+
+describe('greeter function', () => {
+  it('when called with argument John, then returns `Hello, John message`', async () => {
+    const name = 'John';
+
+    const response = await greeter(name);
+
+    expect(response).toBe(`Hello, ${name}`);
+  });
+
+  it('when called without argument, then returns `Hello, John Done message`', async () => {
+    const response = await greeter();
+
+    expect(response).toBe(`Hello, John Doe`);
+  });
+});
