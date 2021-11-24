e97f9016e0c0ec88f6f049ba6af9d7d9de65ca6c
Michal Gosek <michal.gosek@almservices.pl>
2021-11-24 10:39:28 +0100

    draft impl of app entry point.

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,27 @@ export function greeter(name: string) {
   return `Hello, ${name}`;
 }
 
-console.log(greeter("John Smith"));
+console.log(greeter('John Smith'));
+
+
+async function AsyncHello() {
+  return 'Hello';
+}
+
+async function main() {
+  const temp = await AsyncHello();
+  console.log(temp);
+}
+
+main()
+  .then(() => {
+    console.log('Application successfully terminated with exit code: 0');
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+
+
+
